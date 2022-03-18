6510996b36395a377b379edfcdcd636ab595a007
fakerrrrrrr <101823856+fakerrrrrrr@users.noreply.github.com>
2022-03-18 11:49:09 +0700

    Update volume.js

--- volume.js
+++ volume.js
@@ -9,18 +9,18 @@ module.exports = {
     execute(client, message, args) {
         const queue = client.player.getQueue(message.guild.id);
 
-       if (!queue || !queue.playing) return message.channel.send({ content: `${message.author}, There is no music currently playing!. ❌` });
+       if (!queue || !queue.playing) return message.channel.send({ content: `${message.author}, There is no music currently playing!. X` });
 
         const vol = parseInt(args[0]);
 
         if (!vol) return message.channel.send({ content: `Current volume: **${queue.volume}** 🔊\n**To change the volume, with \`1\` to \`${maxVol}\` Type a number between.**` });
 
-        if (queue.volume === vol) return message.channel.send({ content: `${message.author}, The volume you want to change is already the current volume ❌` });
+        if (queue.volume === vol) return message.channel.send({ content: `${message.author}, The volume you want to change is already the current volume X` });
 
-        if (vol < 0 || vol > maxVol) return message.channel.send({ content: `${message.author}, **Type a number from \`1\` to \`${maxVol}\` to change the volume .** ❌` });
+        if (vol < 0 || vol > maxVol) return message.channel.send({ content: `${message.author}, **Type a number from \`1\` to \`${maxVol}\` to change the volume .** X` });
 
         const success = queue.setVolume(vol);
 
-        return message.channel.send({ content: success ? `Volume changed: **%${vol}**/**${maxVol}** 🔊` : `${message.author}, Something went wrong. ❌` }) ;
+        return message.channel.send({ content: success ? `Volume changed: **%${vol}**/**${maxVol}** 🔊` : `${message.author}, Something went wrong. X` }) ;
     },
 };
